52597c67a15380fd07fd7f20ed539185deadd9b9
Franklin Santaella <32347652+frank1411@users.noreply.github.com>
2024-11-08 13:41:47 -0400

    Update ReporteGeneralAfiliacionApplication.java

--- src/main/java/com/fmsl/ReporteGeneralAfiliaciones/ReporteGeneralAfiliacionApplication.java
+++ src/main/java/com/fmsl/ReporteGeneralAfiliaciones/ReporteGeneralAfiliacionApplication.java
@@ -1,11 +1,44 @@
-package com.example.transaction;
+package com.gracaconsultores.ReporteGeneralAfiliaciones;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
-public class TransactionServiceApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(TransactionServiceApplication.class, args);
-    }
+@EnableAsync
+public class ReporteGeneralAfiliacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReporteGeneralAfiliacionApplication.class, args);
+	}
+
+	@Bean("asyncTrackLog")
+	public TaskExecutor getAsyncExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(10);
+		executor.setMaxPoolSize(30);
+		executor.setQueueCapacity(10);
+		executor.setWaitForTasksToCompleteOnShutdown(true);
+		executor.setThreadNamePrefix("LOG-ASYNC");
+		executor.initialize();
+		return executor;
+	}
+
 }
+
+//	@Bean
+//	public Docket api() {
+//		log.info("-- ActualizaAfiliacionApplication.api: ");
+//		return new Docket(DocumentationType.SWAGGER_2)
+//				.apiInfo(new ApiInfo("Service Model - GRACA", "Microservice for BDV - GRACA", "0.1", "termsOfServiceUrl", "luischirino2970@gmail.com", "", "licenseUrl"))
+//				.select()
+//				.apis(RequestHandlerSelectors.any())
+//				.paths(PathSelectors.any())
+//				.paths(Predicates.not(PathSelectors.regex("/error.*")))
+//				.build();
+//	}
+//}
